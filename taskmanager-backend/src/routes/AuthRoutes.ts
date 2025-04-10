b1e4c085d7bba713dece1c9aca36f2efd08223c5
Lucas Santoro <106706839+lucas-santoro@users.noreply.github.com>
2025-04-10 15:02:23 -0300

    Add logout route

--- taskmanager-backend/src/routes/AuthRoutes.ts
+++ taskmanager-backend/src/routes/AuthRoutes.ts
@@ -11,5 +11,6 @@ const router = Router();
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
 router.post("/refresh-token", AuthController.refreshToken);
+router.post("/logout", AuthController.logout);
 
 export default router;
